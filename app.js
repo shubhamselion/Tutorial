7367b4e88bd8e8c7cd49bb7ad55b43e5d0341dad
shubham kumar <shubhamselion@gmail.com>
2026-08-30 12:51:05 +0530

    Add comments to app.js

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ btn.style.backgroundColor="yellow";
 document.querySelector("body").append(btn);
 document.querySelector("h1").style.color="yellow";
 //hello first comment
+
+//pull the request form online repo to local repo
