9e4973fe474630fa323ff3401aa39e644124447c
shubham kumar <shubhamselion@gmail.com>
2026-08-30 12:14:42 +0530

    add event listener

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ let p =document.createElement("p");
 p.innerText="this is the first paragraph";
 let body=document.querySelector("body");
 body.append(p);
+
+btn.addEventListener("click",function(){
+    document.querySelector("body").style.backgroundColor="grey";
+
+});
