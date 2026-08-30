c246088968c20b6943da9bc7ffaab0d409dfb10b
shubham kumar <shubhamselion@gmail.com>
2026-08-30 11:03:48 +0530

    Add paragraph element below button in app.js
    it tell's the website info.

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ let btn = document.createElement("button");
 btn.innerText="click me ";
 btn.style.backgroundColor="yellow";
 document.querySelector("body").append(btn);
+let p =document.createElement("p");
+p.innerText="this is the first paragraph";
+let body=document.querySelector("body");
+body.append(p);
